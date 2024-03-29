3c87c0cafef50f61f8e6a63e4ea4a3734dc333a9
victor-nsengiyumva <jerrysyre@gmail.com>
2024-03-29 12:32:06 +0300

    done creating the objects with the questions

---
+++ lib/src/features/diagnosis/data/questions.dart
@@ -0,0 +1,97 @@
+List screen_1 = [
+  {
+    "symptom": "sadness",
+    "question": "Experience feelings of sadness or low mood",
+    "options": ["USUALLY", "SOMETIMES", "SELDOM", "NEVER"]
+  },
+  {
+    "symptom": "exhausted",
+    "question": "Feeling drained of energy, physically and mentally",
+    "options": ["USUALLY", "SOMETIMES", "SELDOM", "NEVER"]
+  },
+  {
+    "symptom": "mood_swing",
+    "question": "Experience sudden and dramatic changes in your mood",
+    "options": ["YES", "NO"]
+  },
+  {
+    "symptom": "authority_respect",
+    "question": "Generally respect and obey rules set by authority figures",
+    "options": ["YES", "NO"]
+  },
+  {
+    "symptom": "try_explanation",
+    "question": "Typically try to explain your actions or behavior to others",
+    "options": ["YES", "NO"]
+  },
+  {
+    "symptom": "aggressive_response",
+    "question": "Often react angrily or aggressively towards others",
+    "options": ["YES", "NO"]
+  },
+];
+
+List screen_2 = [
+  {
+    "symptom": "ignore_and_move_on",
+    "question":
+        "Tend to avoid or ignore problems instead of dealing with them directly",
+    "options": ["YES", "NO"]
+  },
+  {
+    "symptom": "euphoric",
+    "question": "How often have you felt overly excited or unusually energetic",
+    "options": ["USUALLY", "SOMETIMES", "SELDOM", "NEVER"]
+  },
+  {
+    "symptom": "sleep_dissorder",
+    "question": "Experience difficulty sleeping or staying asleep",
+    "options": ["USUALLY", "SOMETIMES", "SELDOM", "NEVER"]
+  },
+  {
+    "symptom": "anorxia",
+    "question": "Often have concerns about your weight or eating habits",
+    "options": ["YES", "NO"]
+  },
+  {
+    "symptom": "nervous_breakdown",
+    "question":
+        "Ever experienced a nervous breakdown or overwhelming emotional episode",
+    "options": ["YES", "NO"]
+  },
+];
+
+List screen_3 = [
+  {
+    "symptom": "suicidal_thoughts",
+    "question": "Often consider harming yourself or ending your life",
+    "options": ["YES", "NO"]
+  },
+  {
+    "symptom": "admit_mistakes",
+    "question": "Have difficulty admitting when you're wrong",
+    "options": ["YES", "NO"]
+  },
+  {
+    "symptom": "over_thinking",
+    "question":
+        "Find yourself dwelling on negative thoughts or worries excessively",
+    "options": ["YES", "NO"]
+  },
+];
+
+List scaled = [
+  {
+    "symptom": "sexual_activity",
+    "question": "How frequently do you engage in sexual activity",
+  },
+  {
+    "symptom": "concentration",
+    "question":
+        "How well are you able to focus on a task without getting distracted",
+  },
+  {
+    "symptom": "optimism",
+    "question": "How optmistic are you",
+  },
+];
