7ac39a8253ea3ff8ef9b20203219d12d55705d15
victor-nsengiyumva <jerrysyre@gmail.com>
2024-04-02 20:08:01 +0300

    create the tab bar for the histry

--- lib/src/features/history/presentation/history_screen.dart
+++ lib/src/features/history/presentation/history_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:medbridge/src/common_widgets/sizedbox_template.dart';
+import 'package:medbridge/src/common_widgets/text_template.dart';
+import 'package:medbridge/src/features/profile/presentation/profile_widget.dart';
 
 class HistoryScreen extends StatefulWidget {
   const HistoryScreen({super.key});
@@ -10,6 +13,125 @@ class HistoryScreen extends StatefulWidget {
 class _HistoryScreenState extends State<HistoryScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Material(
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            toolbarHeight: 60,
+            scrolledUnderElevation: 0,
+            automaticallyImplyLeading: false,
+            title: TextCustom(
+              text: "History",
+              isBold: true,
+              size: 19,
+              color: Color.fromARGB(255, 8, 33, 99),
+            ),
+            actions: [
+              GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                      backgroundColor: Colors.white,
+                      context: context,
+                      builder: (context) {
+                        return SizedBox(
+                          height: 110,
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 10, right: 10, top: 10, bottom: 10),
+                                child: Container(
+                                  height: 80,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(5.0),
+                                    child: Row(
+                                      children: [
+                                        CircleAvatar(
+                                          backgroundColor:
+                                              Color.fromARGB(255, 8, 33, 99),
+                                          child: TextCustom(
+                                            text: "V",
+                                            color: Colors.white,
+                                            isBold: true,
+                                          ),
+                                          radius: 20,
+                                        ),
+                                        W(w: 15),
+                                        Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            TextCustom(
+                                              size: 18,
+                                              text: "Victor Nsenji",
+                                              isBold: true,
+                                            ),
+                                            TextCustom(
+                                              size: 15,
+                                              text:
+                                                  "nsengiyumvavictor@gmail.com",
+                                              color: Color.fromARGB(
+                                                  255, 122, 122, 122),
+                                            ),
+                                          ],
+                                        ),
+                                        Spacer(),
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(right: 5),
+                                          child: InkWell(
+                                            onTap: () {},
+                                            child: Icon(
+                                              Icons.logout,
+                                              color: Colors.red,
+                                              size: 30,
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 10.0),
+                  child: ProfileWidget(),
+                ),
+              )
+            ],
+            bottom: const TabBar(
+              labelColor: Color.fromARGB(255, 8, 33, 99),
+              indicatorColor: Color.fromARGB(255, 8, 33, 99),
+              tabs: [
+                Tab(
+                  text: "Test Results",
+                ),
+                Tab(
+                  text: "Receipts",
+                ),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Center(
+                child: Text("test results"),
+              ),
+              Center(
+                child: Text("receipts"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
